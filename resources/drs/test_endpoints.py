daeaf0300787743c720e88508a3f079fb468460e
Seif Ashraf <63073172+seifashraf1@users.noreply.github.com>
2023-04-12 16:06:38 +0200

    Update test_endpoints.py

--- resources/drs/test_endpoints.py
+++ resources/drs/test_endpoints.py
@@ -7,14 +7,14 @@ from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 from utils import *
 
-class TestEndpoints(unittest.TestCase):
+class TestEndpoints(unittest.TestCase):  
     
-    def test_success_objects(self):
+    def test_successful_request(self):
         for object_id in SUCCESS_STATUS_OBJECTS:
             response = requests.get(endpoint_url.replace("{object_id}", object_id))
             self.assertEqual(response.status_code, 200, f"Failed to retrieve DRS object. Response status code: {response.status_code}")
     
-    def test_failure_objects(self):
+    def test_failed_request(self):
         for object_id in FAILURE_STATUS_OBJECTS:
             response = requests.get(endpoint_url.replace("{object_id}", object_id))
             self.assertEqual(response.status_code, 404, f"Expected a 404 status code for nonexistent object, but got {response.status_code}")
@@ -42,25 +42,34 @@ class TestEndpoints(unittest.TestCase):
             except jsonschema.exceptions.ValidationError:
                 pass # Expecting schema validation to fail for nonexistent object, so no action needed
 
-    def test_success_access_methods(self):
+    def test_missing_access_url_or_id(self):
         for object_id in SUCCESS_STATUS_OBJECTS:
             response = requests.get(endpoint_url.replace("{object_id}", object_id))
-            #get access id from access method and check the access id is in the response
-            access_id = response.json().get("access_methods")[0].get("access_id")
-            self.assertTrue(access_id in response.json().get("access_methods")[0].get("access_url").get("url"), "Expected 'access_id' to be in 'access_url.url'")
-            self.assertTrue(response.json(), "Failed to retrieve DRS object")
-            self.assertTrue(response.json().get("access_methods"), "Expected 'access_methods' to be populated")
-            for access_method in response.json().get("access_methods"):
-                self.assertTrue(access_method.get("access_url"), "Expected 'access_url' to be populated")
-                self.assertTrue(access_method.get("type"), "Expected 'type' to be populated")
-                self.assertTrue(access_method.get("access_id"), "Expected 'access_id' to be populated")
-                self.assertTrue(access_method.get("region"), "Expected 'region' to be populated")
-                self.assertTrue(access_method.get("url"), "Expected 'url' to be populated")
-                self.assertTrue(access_method.get("headers"), "Expected 'headers' to be populated")
-                self.assertTrue(access_method.get("http_method"), "Expected 'http_method' to be populated")
-                self.assertTrue(access_method.get("access_url").get("url"), "Expected 'access_url.url' to be populated")
-                self.assertTrue(access_method.get("access_url").get("headers"), "Expected 'access_url.headers' to be populated")
-                self.assertTrue(access_method.get("access_url").get("http_method"), "Expected 'access_url.http_method' to be populated")
+            response_json = response.json()
+            self.assertIsNotNone(response_json, "Access API did not return a valid JSON response")
+            self.assertNotIn("access_url", response_json, "Access API returned unexpected access_url field")
+            self.assertNotIn("access_id", response_json, "Access API returned unexpected access_id field")
+    
+    def test_access_id_with_valid_access_url(self):
+        for object_id in SUCCESS_STATUS_OBJECTS:
+            response = requests.get(endpoint_url.replace("{object_id}", object_id))
+            response_json = response.json()
+            access_url = response_json.get("access_url")
+            if access_url:
+                self.assertIsNotNone(access_url, "Access API did not return a valid access_url for access_id")
+                response = requests.get(access_url)
+                self.assertEqual(response.status_code, 200, "Access API returned invalid access_url for access_id")    
+
+    def test_access_id_with_invalid_access_url(self):
+        for object_id in SUCCESS_STATUS_OBJECTS:
+            response = requests.get(endpoint_url.replace("{object_id}", object_id))
+            response_json = response.json()
+            access_id = response_json.get("access_id")
+            if access_id:
+                invalid_access_url = "http://invalid_access_url"
+                response = requests.get(invalid_access_url)
+                self.assertNotEqual(response.status_code, 200, "Access API returned valid access_url for invalid access_id")
+    
     
     def print_request(self, request):
         print('Request: {}'.format(request))
